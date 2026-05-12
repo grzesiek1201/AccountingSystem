d645d97625419c3ed171589078db9b47d82de9f3
grzesiek1201 <gzywicki@gmail.com>
2026-05-12 23:23:43 +0200

    Add Product and Category models

---
+++ AccountingSystem/Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AccountingSystem.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
